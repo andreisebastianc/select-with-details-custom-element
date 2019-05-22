9d4c4ef3911f0a08bdd26cf4f9f5e16f1c8ee30b
Andrei Sebastian Cîmpean <andreisebastianc@gmail.com>
2019-05-22 22:18:13 +0300

    Display work on the component

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import initializeCustomElements from '@glimmer/web-component';
 import App from './main';
 
 const app = new App();
-const containerElement = document.getElementById('app');
 
 setPropertyDidChange(() => {
   app.scheduleRerender();
@@ -14,8 +13,6 @@ app.registerInitializer({
     registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
   }
 });
-
-app.renderComponent('SelectWithDetails', containerElement, null);
 
 app.boot();
 initializeCustomElements(app, { 'select-with-details': 'SelectWithDetails' });
--- src/ui/components/SelectWithDetails/component.ts
+++ src/ui/components/SelectWithDetails/component.ts
@@ -1,5 +1,63 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+
+interface Option {
+  title: string;
+  price?: string;
+  description?: string;
+  id: string;
+}
+
+const tempOptions: Array<Option> = [
+  {
+    title: "Vanilie cu fulgi de ciocolată",
+    price: "100 RON",
+    id: "xxx",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat \
+  mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
+  }, 
+  {
+    title: "Buttercream ciocolată",
+    price: "100 RON",
+    id: "yyy",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat \
+  mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
+  },
+  {
+    title: "Vanilie cu fulgi de ciocolată",
+    price: "100 RON",
+    id: "zzz",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat \
+  mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
+  }, 
+  {
+    title: "Buttercream ciocolată",
+    price: "100 RON",
+    id: "ttt",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat \
+  mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
+  }
+]
 
 export default class SelectWithDetails extends Component {
+  @tracked isExpanded = false;
+  @tracked selected: Option ;
+  @tracked options: Array<Option> = tempOptions;
 
+  constructor(options) {
+    super(options);
+    this.selected = tempOptions[0];
+  }
+
+  expand() {
+    this.isExpanded = true;
+  }
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  select(selected) {
+    this.selected = selected;
+    this.collapse();
+  }
 }
